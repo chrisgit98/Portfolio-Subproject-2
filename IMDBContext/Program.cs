94c18b4cca59184bd7c49ac5f1bfec760ddc229d
Theofylakt <theofylakt@ruc.dk>
2021-11-11 14:07:00 +0100

    connected the similar_movies()

--- IMDBContext/Program.cs
+++ IMDBContext/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using EfEx;
 using Xunit;
 using System.Collections.Generic;
+using Npgsql;
 
 namespace EfEx
 {
@@ -14,19 +15,36 @@ namespace EfEx
 
         public static void Main()
         {
-            var dataService = new DataService();
+            //var dataService = new DataService();
 
-            var name_basics = dataService.GetNames();
+            //var name_basics = dataService.GetNames();
 
-            foreach (var names in name_basics)
-            {
-                Console.WriteLine(names.PersonName);
-            }
+            //foreach (var names in name_basics)
+            //{
+            //    Console.WriteLine(names.PersonName);
+            //}
 
             //var dataService = new DataService();
             //var film = dataService.GetTitleBasics();
             //Console.WriteLine(film.FilmId);
 
+            var connectionString = "host=localhost;db=imdb_small;uid=postgres;pwd=@DAc43712";
+            UseAdo(connectionString);
+
+        }
+
+        private static void UseAdo(string connectionString)
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new NpgsqlCommand("SELECT * FROM SIMILAR_MOVIES('tt0372784')", connection);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetString(0)}");
+            }
         }
     }
 
