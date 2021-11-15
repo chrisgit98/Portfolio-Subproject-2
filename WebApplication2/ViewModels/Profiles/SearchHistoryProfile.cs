daa7a8fb61cbe055e7dea655ee3a36dabd609b22
chrisgit98 <90904750+chrisgit98@users.noreply.github.com>
2021-11-15 14:43:04 +0100

    added profile

---
+++ WebApplication2/ViewModels/Profiles/SearchHistoryProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EfEx;
+using EfEx.Domain
+
+namespace WebService.ViewModels.Profiles
+{
+    public class SearchHistoryProfile : Profile
+    {
+        public SearchHistoryProfile()
+        {
+            CreateMap<SearchHistory, SearchHistoryViewModel>();
+            CreateMap<CreateSearchHistoryViewModel, SearchHistory>();
+        }
+    }
+}
