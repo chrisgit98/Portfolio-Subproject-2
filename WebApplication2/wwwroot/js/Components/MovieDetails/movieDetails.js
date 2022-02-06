a0c44129386d9da060eecd0a326c3b41720ac19d
Theofylakt <theofylakt@ruc.dk>
2022-02-06 16:06:14 +0100

    rating star system

--- WebApplication2/wwwroot/js/Components/MovieDetails/movieDetails.js
+++ WebApplication2/wwwroot/js/Components/MovieDetails/movieDetails.js
@@ -69,7 +69,28 @@
             console.log(bookmarkTitle);
             bs.deleteBookmarkTitle(bookmarkTitle);
         }
+       //adding the star system
+        var $star_rating = $('.star-rating .fa');
 
+        var SetRatingStar = function () {
+            return $star_rating.each(function () {
+                if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+                    return $(this).removeClass('fa-star-o').addClass('fa-star');
+                } else {
+                    return $(this).removeClass('fa-star').addClass('fa-star-o');
+                }
+            });
+        };
+
+        $star_rating.on('click', function () {
+            $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+            return SetRatingStar();
+        });
+
+        SetRatingStar();
+        $(document).ready(function () {
+
+        });
         return {
             titleOtherview,
             similarMovies,
